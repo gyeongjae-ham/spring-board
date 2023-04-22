44cb0f8957b7f24234810df64749969fded2b9ac
hiyee-gj <gyeongjae.h.dev@gmail.com>
2023-04-22 23:38:37 +0900

    #14 - 회원 계정 도메인 구현 및 레포지토리 생성
    `UserAccount` 이름으로 회원 계정 도메인 생성
    `user`, `account` 등은 mysql 예약어이므로 피함
    필드명도 mysql 예약어를 의식하여 정함
    
    테스트 데이터에 임의 계정 1개 등록
    테스트용이지만 패스워드가 노출되는 방식이므로 고민 필요
    
    회원 정보는 API 로 노출되지 않았으면 한다.
    이에 테스트에 404 not found 를 확인하는 테스트 추가
    간단하게 모든 http 메소드에 대응

---
+++ src/main/java/com/fastcampus/springboard/repository/UserAccountRepository.java
@@ -0,0 +1,7 @@
+package com.fastcampus.springboard.repository;
+
+import com.fastcampus.springboard.domain.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+}
