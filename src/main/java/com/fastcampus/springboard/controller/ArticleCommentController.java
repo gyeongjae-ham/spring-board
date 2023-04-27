51043234a65bd91914d031942b5fa2e007602d0b
hiyee-gj <gyeongjae.h.dev@gmail.com>
2023-04-27 18:56:10 +0900

    #42 - 댓글 저장, 삭제 기능을 위한 컨트롤러와 서비스 코드

---
+++ src/main/java/com/fastcampus/springboard/controller/ArticleCommentController.java
@@ -0,0 +1,35 @@
+package com.fastcampus.springboard.controller;
+
+import com.fastcampus.springboard.dto.UserAccountDto;
+import com.fastcampus.springboard.dto.request.ArticleCommentRequest;
+import com.fastcampus.springboard.service.ArticleCommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@RequestMapping("/comments")
+@Controller
+public class ArticleCommentController {
+
+    private final ArticleCommentService articleCommentService;
+
+    @PostMapping("/new")
+    public String postNewArticleComment(ArticleCommentRequest articleCommentRequest) {
+        // TODO: 인증 정보를 넣어줘야 한다.
+        articleCommentService.saveArticleComment(articleCommentRequest.toDto(UserAccountDto.of(
+                "luca", "pw", "luca@email.com", null, null
+        )));
+
+        return "redirect:/articles/" + articleCommentRequest.articleId();
+    }
+
+    @PostMapping("/{commentId}/delete")
+    public String deleteArticleComment(@PathVariable Long commentId, Long articleId) {
+        articleCommentService.deleteArticleComment(commentId);
+
+        return "redirect:/articles/" + articleId;
+    }
+}
